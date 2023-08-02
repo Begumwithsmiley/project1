7b19f14933330e41d4cb3f183a1ed814384e01b8
Begumwithsmiley <begumsener94@gmail.com>
2023-08-02 20:11:44 +0300

    updates

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+
+export default function SignupPage() {
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    })
+    const onSignup = async () => {
+
+    }
+
+    return (
+
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-center text-2xl">Signup</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input
+                className="p-3 border border-pink-500 rounded-lg mb-4 focus:outline-none focus:border-pink-500"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+
+            <label htmlFor="username">password</label>
+            <input
+                className="p-3 border border-pink-500 rounded-lg mb-4 focus:outline-none focus:border-pink-500"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+
+            <label htmlFor="email">email</label>
+            <input
+                className="p-3 border border-pink-500 rounded-lg mb-4 focus:outline-none focus:border-pink-500"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+            
+            <button onClick={onSignup} className="p-3 border border-pink-500 rounded-lg mb-4 focus:outline-none focus:border-pink-500">Signup here</button>
+            <Link href="/login">Visit login page</Link>
+
+        </div>
+    )
+}
